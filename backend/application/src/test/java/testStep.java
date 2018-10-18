cce4cf5a0a616bde675581b0b62e45a4a5ee2b38
mehmetyilmaz <mehmet.yilmaz02@rabobank.nl>
2018-10-18 13:46:52 +0200

    Added some feature files

---
+++ backend/application/src/test/java/testStep.java
@@ -0,0 +1,9 @@
+import cucumber.api.PendingException;
+
+public class testStep {
+//    public testStep() {
+//        Given("^de website tour of heroes$", () -> {
+//            // Write code here that turns the phrase above into concrete actions
+//        });
+//    }
+}
--- backend/application/src/test/resources/first.feature
+++
@@ -1,9 +0,0 @@
-#language: nl
-
-Feature: Into
-  First feature file
-
-  Scenario: navigeer door tour of heroes
-  Gegeven de website tour of heroes
-  Als ik op Magneta klik
-  Dan zie ik de details van Magneta staan
